e4cd3dd0721e3f58afe4997fe0561aca5d5b1806
Atul Gupta <atul4a2l@gmail.com>
2019-12-18 18:41:48 +0530

    Added appService for getting json data

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,52 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-interface Result {
-  name:string,
-  marks:{
-      Maths:string,
-      English:string,
-      Science:string
-  },
-  rollNumber:string
-}
+import { Component } from '@angular/core';
+import { AppServiceService } from '../app-service.service'
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent implements OnInit {
-  newResult=[];
-  resultData :Result[]= [
-    {
-        "name":"rajiv",
-        "marks":{
-            "Maths":"18",
-            "English":"21",
-            "Science":"45"
-        },
-        "rollNumber":"KV2017-5A2"
-    },
-    {
-        "name":"abhishek",
-        "marks":{
-            "Maths":"43",
-            "English":"30",
-            "Science":"37"
-        },
-        "rollNumber":"KV2017-5A1"
-    },
-    {
-        "name":"zoya",
-        "marks":{
-            "Maths":"42",
-            "English":"31",
-            "Science":"50"
-        },
-        "rollNumber":"KV2017-5A3"
-    }
-]
+export class ResultComponent  {
+  results=[];
 
-  constructor() { }
+  constructor(private appService:AppServiceService) {
+    this.appService.getStudentResult().subscribe(response=>this.checkResult(response))
+   }
 
   checkResult(arr){
   arr.forEach(element => {
@@ -54,17 +19,13 @@ export class ResultComponent implements OnInit {
       return parseInt(a) + parseInt(b);
     });
     element.totalMarks=sum;
-    this.newResult.push(element)
+    this.results.push(element)
     const isBelowThreshold = (currentValue) => currentValue > 20;
     element.status=Object.values(element.marks).every(isBelowThreshold)==true?'Pass':'Fail'
     });
-    const max = this.newResult.reduce((a, b) =>{
+    const max = this.results.reduce((a, b) =>{
       return (a.totalMarks > b.totalMarks) ? a : b
     })
     max.status='Topper'
-  }
-
-  ngOnInit() {
-    this.checkResult(this.resultData)
   }
 }
